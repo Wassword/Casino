053413262daa231b8d954067896397cd14b9f2b2
TylerJac <sans.gaming235@gmail.com>
2024-10-21 13:13:26 -0400

    Update UserDetailsServiceImpl.java

--- src/main/java/org/casino/service/UserDetailsServiceImpl.java
+++ src/main/java/org/casino/service/UserDetailsServiceImpl.java
@@ -13,24 +13,20 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Set<GrantedAuthority> authorities = user.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
-                .collect(Collectors.toSet());
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                authorities
-        );
-    }
-}
+//@Service
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//   @Override
+//public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//    User user = userRepository.findByUsername(username)
+//            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//
+//    return new org.springframework.security.core.userdetails.User(
+//            user.getUsername(),
+//            user.getPassword(),
+//            user.getAuthorities()
+//    );
+//}
+//}
