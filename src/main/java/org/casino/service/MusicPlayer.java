44d13fa9897003e25d5ce8ae9c79bd4a0255eb87
Saba Ahmed <ahmedsaba271@gmail.com>
2024-10-28 14:11:39 -0400

    added music

--- src/main/java/org/casino/service/MusicPlayer.java
+++ src/main/java/org/casino/service/MusicPlayer.java
@@ -1,20 +1,39 @@
+package org.casino.service;
+
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import java.net.URL;
+
 public class MusicPlayer {
+
     private MediaPlayer mediaPlayer;
 
+    // Method to play background music
     public void playBackgroundMusic() {
-        String musicFile = getClass().getClassLoader().getResource("music/Quirky-Dog(chosic.com).mp3").toString();
-        Media sound = new Media(musicFile);
-        mediaPlayer = new MediaPlayer(sound);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); // Loop the music indefinitely
-        mediaPlayer.play();
+        try {
+            // Load the music file from the resources directory
+            URL resource = getClass().getClassLoader().getResource("music/Quirky-Dog(chosic.com).mp3");
+
+            // Check if the resource is available
+            if (resource != null) {
+                String musicFile = resource.toString(); // Convert URL to String
+                Media sound = new Media(musicFile);
+                mediaPlayer = new MediaPlayer(sound);
+                mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE); // Loop the music indefinitely
+                mediaPlayer.play(); // Start the music
+            } else {
+                System.out.println("Music file not found!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Print error stack trace for debugging
+        }
     }
 
+    // Method to stop background music
     public void stopBackgroundMusic() {
         if (mediaPlayer != null) {
-            mediaPlayer.stop();
+            mediaPlayer.stop(); // Stop the music if it's playing
         }
     }
 }
